d358fc3d7d1810e35278669d9535ecc62effbd56
hellgate75 <hellgate75@gmail.com>
2020-03-07 10:43:01 +0000

    Moving modules to external repo

--- server/proxy/proxy.go
+++ server/proxy/proxy.go
@@ -1,35 +1,11 @@
 package proxy
 
 import (
-	"errors"
-	"fmt"
+	"github.com/hellgate75/go-tcp-modules/server/proxy"
 	"github.com/hellgate75/go-tcp-server/common"
-	"github.com/hellgate75/go-tcp-server/server/proxy/shell"
-	"github.com/hellgate75/go-tcp-server/server/proxy/transfer"
-	"strings"
+
 )
 
-var commandsMap map[string]common.Commander = make(map[string]common.Commander)
-
-var filled bool = false
-
-func initMap() {
-	commandsMap["transfer-file"] = transfer.New()
-	commandsMap["shell"] = shell.New()
-	filled = true
-}
-
 func GetCommander(command string) (common.Commander, error) {
-	if !filled {
-		initMap()
-	}
-	var cmd string = strings.TrimSpace(strings.ToLower(command))
-	if commander, ok := commandsMap[cmd]; ok {
-		if commander == nil {
-			return nil, errors.New(fmt.Sprintf("Unable to collect command: %s", cmd))
-		}
-		return commander, nil
-	} else {
-		return nil, errors.New(fmt.Sprintf("Command unavailable: %s", cmd))
-	}
+	return proxy.GetCommander(command)
 }
